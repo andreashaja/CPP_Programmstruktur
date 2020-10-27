f56b77081007bb0a20d9821218e3d6441e2270f9
ahaja <ah@andreashaja.com>
2020-10-27 08:51:48 +0100

    Update preproc.cpp

--- preproc.cpp
+++ preproc.cpp
@@ -2,24 +2,24 @@
 #include <iostream>
 
 #define PI 3 // 3.14  // 3.141592654
-#define USE_SIN
+#define USE_COS
 
 int main()
 {
   #ifdef __unix__  
   std::cout << "Unix" << std::endl;
   #elif __APPLE__
-  std::cout << "Mac OS" << std::endl
+  std::cout << "Mac OS" << std::endl;
   #elif
   std::cout << "anderes OS" << std::endl
   #endif
 
   #ifdef USE_SIN
-  std::cout << "sin(pi) = " << std::sin(PI) << std::endl;
+  std::cout << "sin(pi) = " << sin(PI) << std::endl;
   #endif
 
   #ifdef USE_COS
-  std::cout << "cos(pi) = " << std::cos(PI) << std::endl;
+  std::cout << "cos(pi) = " << cos(PI) << std::endl;
   #endif
 
   return 0;
